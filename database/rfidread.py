698058b28cdfe17b4ac75a2ff1337ff75e23520d
Bboy Mega <matrixous@bboy.live>
2021-12-11 14:23:50 +0800

    Add files via upload

--- database/rfidread.py
+++ database/rfidread.py
@@ -11,10 +11,10 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE IF NOT EXISTS records (rfid VARCHAR(64), num VARCHAR(10), date_entry VARCHAR(128))") #Create Database
+mycursor.execute("CREATE TABLE IF NOT EXISTS records (count VARCHAR(15), rfid VARCHAR(64), num VARCHAR(10), date_entry VARCHAR(128))") #Create Database
 response = urlopen("http://127.0.0.1") #Get Json data from API
 data_json = json.loads(response.read()) #Load json data from HTTP GET
-sql = "INSERT INTO records (rfid, num , date_entry) VALUES (%s, %s, %s)"
-val = (data_json["info"]["RFID"], data_json["info"]["Num"], datetime.now())
+sql = "INSERT INTO records (count, rfid, num , date_entry) VALUES (%s, %s, %s, %s)"
+val = (data_json["info"]["COUNT"], data_json["info"]["RFID"], data_json["info"]["Num"], datetime.now())
 mycursor.execute(sql, val)
 mydb.commit()
